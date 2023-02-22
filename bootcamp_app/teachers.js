390d5e42f365c8ec98aeb98b5882964c589d897c
Daniel Seniv <danielseniv@outlook.com>
2023-02-22 07:37:38 +0000

    Add same pg database connection, add 1 qury

---
+++ bootcamp_app/teachers.js
@@ -0,0 +1,30 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx',
+  port: 5432
+});
+
+pool
+  .connect()
+  .then(() => console.log(`DB is running on port ${pool.port}`));
+
+
+pool.query(`
+  SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+  FROM assistance_requests
+  JOIN teachers ON teachers.id = teacher_id
+  JOIN students ON students.id = student_id
+  JOIN cohorts ON cohorts.id = cohort_id
+  WHERE cohorts.name = 'JUL02'
+  ORDER BY teachers.name;
+`)
+.then(res => {
+  res.rows.forEach(user => {
+    console.log(`${user.cohort}: ${user.teacher}`);
+  })
+})
+.catch(err => console.error('query error', err.stack));
