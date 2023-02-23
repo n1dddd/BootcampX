8262bb5afdb6e206ed3f33f718a96ee8dd1980b0
Daniel Seniv <danielseniv@outlook.com>
2023-02-23 05:22:31 +0000

    Change queries to parameterized queries

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -8,20 +8,22 @@ const pool = new Pool({
   port: 5432
 });
 
+const queryString = `
+SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+FROM assistance_requests
+JOIN teachers ON teachers.id = teacher_id
+JOIN students ON students.id = student_id
+JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohorts.name = 'JUL02'
+ORDER BY teachers.name;
+`
+
 pool
   .connect()
   .then(() => console.log(`DB is running on port ${pool.port}`));
 
 
-pool.query(`
-  SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
-  FROM assistance_requests
-  JOIN teachers ON teachers.id = teacher_id
-  JOIN students ON students.id = student_id
-  JOIN cohorts ON cohorts.id = cohort_id
-  WHERE cohorts.name = 'JUL02'
-  ORDER BY teachers.name;
-`)
+pool.query(queryString)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.cohort}: ${user.teacher}`);
